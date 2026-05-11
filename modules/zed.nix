51b7ac80afa21b6f58338394aadd7758f17beb45
Vincent Wong <vincent@vwong.dev>
2026-05-12 01:00:40 +1000

    fix(zed): Specify font

--- modules/zed.nix
+++ modules/zed.nix
@@ -4,6 +4,10 @@
     enable = true;
     extensions = [ "java" ];
     userSettings = {
+      ui_font_size = 16;
+      ui_font_family = "MesloLGS NF";
+      buffer_font_size = 15;
+      buffer_font_family = "MesloLGS NF";
       lsp = {
         jdtls = {
           binary = {
