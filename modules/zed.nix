4dce985cc04fa82f333a471234261c99d07d875e
Vincent Wong <vincent@vwong.dev>
2026-05-13 20:58:04 +1000

    feat(zed): Decrease font size

--- modules/zed.nix
+++ modules/zed.nix
@@ -4,9 +4,9 @@
     enable = true;
     extensions = [ "java" ];
     userSettings = {
-      ui_font_size = 16;
+      ui_font_size = 12;
       ui_font_family = "MesloLGS NF";
-      buffer_font_size = 15;
+      buffer_font_size = 11;
       buffer_font_family = "MesloLGS NF";
       lsp = {
         jdtls = {
